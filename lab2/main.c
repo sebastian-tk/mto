5c8247bc21c6217655a4ff265d69b01bd4e0092d
sebastian-tk <sebastian.tkaczykk@gmail.com>
2022-10-09 14:58:56 +0200

    Add convert to number

--- lab2/main.c
+++ lab2/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void convert_letters(char *param)
+
+
+void swapLetters(char *param)
 {
 	for (int j = 0; j < strlen(param); j++)
 	{
@@ -16,62 +20,84 @@ void convert_letters(char *param)
 	}
 }
 
-char* cutoff(const char* str, int from , int to)
+int convertToNumber(const char *str)
 {
-    if (from >= to)
-        return  NULL;
-
-    char* cut = calloc(sizeof(char), (to - from) + 1);
-    char* begin = cut;
-    if (!cut)
-        return  NULL;
-
-    const char* fromit = str+from;
-    const char* toit = str+to;
-    (void)toit;
-    memcpy(cut, fromit, to);
-    return begin;
+	int numb;
+	for (int i = 0; isFinishChars(str, i); i++)
+	{
+		if (i == 0)
+		{
+			numb = (int)str[i];
+		}
+		numb = numb * 10 + (int)str[i];
+	}
+	return numb;
 }
 
-bool isFinishChars(char *param, int idx)
+char *cutoff(const char *str, int from, int to)
 {
-	return idx < strlen(format_string)
+	if (from >= to)
+		return NULL;
+
+	char *cut = calloc(sizeof(char), (to - from) + 1);
+	char *begin = cut;
+	if (!cut)
+		return NULL;
+
+	const char *fromit = str + from;
+	const char *toit = str + to;
+	(void)toit;
+	memcpy(cut, fromit, to);
+	return begin;
+}
+
+bool isFinishChars(char *format_string, int idx)
+{
+	return idx < strlen(format_string);
 }
 
 int my_printf(char *format_string, char *param)
 {
-	for (int i = 0; isFinishChars(format_string,i); i++)
+	for (int i = 0; isFinishChars(format_string, i); i++)
 	{
 		if ((format_string[i] == '#') && (format_string[i + 1] == 'k'))
 		{
 			i++;
-			convert_letters(param);
+			swapLetters(param);
 			printf("%s", param);
 		}
-		esle if ((format_string[i] == '#') && (format_string[i + 1] == '.'))
+		else if ((format_string[i] == '#') && (format_string[i + 1] == '.'))
 		{
 			i += 2;
 			int number;
 			int start = i + 1;
 			int finish = -1;
-			while (param[i] >= '0' && param[i] <= '9' && isFinishChars(format_string,i))
+			while (format_string[i] >= '0' && format_string[i] <= '9' && isFinishChars(format_string, i))
 			{
-				if (isFinishChars(format_string,i))
+				if (isFinishChars(format_string, i))
 				{
 					finish = i;
 				}
 				i++;
 			}
 
-			if(param[i] == 'k'){
-
+			if (finish != -1 && format_string[i] == 'k')
+			{
+				int val = convertToNumber(cutoff(format_string, start, finish));
+				if (val < strlen(param))
+				{
+					param = cutoff(param, 0, val);
+				}
+				swapLetters(param);
+				printf("%s", param);
 			}
 
-			convert_letters(param);
+			swapLetters(param);
 			printf("%s", param);
 		}
 
-		else putchar(format_string[i]);
+		else
+			putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
