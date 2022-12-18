4e341db51c356610910dcf805593234518b0a8d3
sebastian-tk <sebastian.tkaczykk@gmail.com>
2022-12-18 15:04:21 +0100

    Add main functions

--- lab7/main.java
+++ lab7/main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 
 class Main {
-    
+
     public static int convertStringToInt(String exp){
         return Integer.parseInt(exp);
     }
@@ -11,14 +11,35 @@ class Main {
     public static String convertIntToHex(int val) {
         return Integer.toHexString(val);
     }
-    
+
+    public static String swapLetter(String expression) {
+        StringBuilder stringBuilder = new StringBuilder(expression);
+        for (int i = 0; i < expression.length() ; i++) {
+            stringBuilder.append(mapChar(expression.charAt(i)));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static char mapChar(char sign) {
+        char convertedChar = sign;
+        switch (sign){
+            case 'a': convertedChar = 'g'; break;
+            case 'b': convertedChar = 'h'; break;
+            case 'c': convertedChar = 'i'; break;
+            case 'd': convertedChar = 'j'; break;
+            case 'e': convertedChar = 'k'; break;
+            case 'f': convertedChar = 'l'; break;
+        }
+        return convertedChar;
+    }
+
     public static void my_printf(String format_string, String param){
         for(int i=0;i<format_string.length();i++){
             if(format_string.charAt(i) == '#' && format_string.charAt(i+1) == 'j'){
                 i++;
                 int number = convertStringToInt(param);
-                String hexValue = convertIntToHex(number);
-
+                String hexValue = swapLetter(convertIntToHex(number));
+                System.out.print(hexValue);
             }else{
                 System.out.print(format_string.charAt(i));
             }
